e70781b28e1e1f016f1aae3df5282282a0aec6df
SergeyRusin83 <sergei-rusin@yandex.ru>
2024-01-15 21:48:27 +0300

    fkgasbgzb

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,6 +6,17 @@ int main()
     setlocale(LC_ALL, "ru");
     cout << "Hello, World!\n";
     cout << "Привет, Мир!\n";
+
+    cout << "Здесь могла быть ваша реклама!" << endl;
+
+    int a, b;
+
+    a = 2;
+    b = ++a * a++;
+
+    cout << a << "  " << b << endl;
+
+    return 0;
     
 }
 
